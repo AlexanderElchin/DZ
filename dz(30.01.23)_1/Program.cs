38e09396953e24355dd31779255f531a6f36d0a6
Alexander <elchinalexander@gmail.com>
2023-01-30 14:09:50 +0300

    dz_30/01/23_1

---
+++ dz(30.01.23)_1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное 
+число и на выходе показывает вторую цифру этого числа.*/
+Console.Clear();
+
+Console.Write("Введите 3-х значное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int amount = a.ToString().Length;
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    int a1 = ((a / 10) % 10);
+    Console.WriteLine(a1);
+}
