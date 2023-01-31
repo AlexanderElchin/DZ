44de92a8791b5b092a0d97b367e42a4091f6ff62
Alexander <elchinalexander@gmail.com>
2023-01-31 21:09:32 +0300

    dz(30.01.23)_2

---
+++ dz(30.01.23)_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number;
+while (number > 1000)
+    number = number / 10;
+if (number < 100)
+    Console.WriteLine("В введеном числе нет третьей цифры");
+else
+{
+    int a1 = number % 10;
+    Console.WriteLine($"Третья цифра чилса {a} - {a1}");
+}
+
+
+
+
