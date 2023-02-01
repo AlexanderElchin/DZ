2370c75e168f11d1fcad2f9bf83d888c21f0e72c
Alexander <elchinalexander@gmail.com>
2023-02-01 08:30:13 +0300

    dz(30.01.23)_4 done

--- dz(30.01.23)_4/Program.cs
+++ dz(30.01.23)_4/Program.cs
@@ -2,20 +2,23 @@
 // Требуется определить значение второго по величине элемента 
 // в этой последовательности, то есть элемента, который будет наибольшим,
 // если из последовательности удалить наибольший элемент.
+Console.Clear();
+Console.WriteLine("Введите число (ввод завершите 0):  ");
 int n = Convert.ToInt32(Console.ReadLine());
 int max1 = 0;
 int max2 = 0;
 while (n != 0)
 {
+    n = Convert.ToInt32(Console.ReadLine());
     if (max1 < n)
     {
         max2 = max1;
         max1 = n;
     }
-    n = Convert.ToInt32(Console.ReadLine());
+
     if (n > max2 && n < max1)
     {
         max2 = n;
     }
 }
-Console.Write(max2);
+Console.WriteLine($"максимальное второе число:{max2}");
