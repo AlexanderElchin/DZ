90e54fc0d09f55547d667d964e55cad5480e8335
Alexander <elchinalexander@gmail.com>
2023-02-04 19:44:22 +0300

    dz(02.01.23)_3

---
+++ dz(02.01.23)_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход пятизначное 
+// число и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.Write("Введите 5ти значное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 9999 || n > 100000)
+{
+    Console.Write("Вы ошиблись! \nвведите 5ти значное число ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n / 10000 == n % 10 && (n / 1000) % 10 == (n / 10) % 10)
+    Console.Write("Это палиндром!");
+else
+    Console.Write("Это не палиндром!");
+
+
