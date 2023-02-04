df3274d39c1754a0a50c2e3ca94ac42f304e396f
Alexander <elchinalexander@gmail.com>
2023-02-04 19:44:06 +0300

    dz(02.01.23)_2

---
+++ dz(02.01.23)_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает 
+// на вход координаты двух точек и находит 
+// расстояние между ними в 3D пространстве.
+Console.Clear();
+Console.Write("Введите координату X: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату X: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Z: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+double l = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 2);
+Console.WriteLine($"Результат: {l}");
