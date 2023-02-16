c1ad357a3719061a014030cbe0ae714b1aaaeee4
Alexander <elchinalexander@gmail.com>
2023-02-16 08:34:49 +0300

    zadacha_41

---
+++ zadacha_41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+Console.WriteLine($"Введите M чисел через пробел: ");
+double[] array = Array.ConvertAll(Console.ReadLine()!.Split(" "), double.Parse); // ввод массива с клавиатуры без обозначения длинны
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество элементов больше 0 : {count}");
